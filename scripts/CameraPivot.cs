a355672b7e26f5e4f9c3b63f522a9aeadd6629af
Widmo206 <widmo206@yahoo.com>
2026-06-14 07:58:02 +0200

    Add collision detection to player camera
    It still has some clipping but not sure how to fix

--- scripts/CameraPivot.cs
+++ scripts/CameraPivot.cs
@@ -4,14 +4,34 @@ using System;
 public partial class CameraPivot : Node3D
 {
 	[Export] public Node3D target;
-	[Export] public float zoomSpeed    = 1f;
-	[Export] public float zoomMin      = 2f;
-	[Export] public float zoomMax      = 30f;
-	[Export] public float sensitivityX = 1f;
-	[Export] public float sensitivityY = 1f;
+	[Export] public float verticalOffset = 1f;
+	[Export] public float zoomSpeed      = 1f;
+	[Export] public float zoomMin        = 2f;
+	[Export] public float zoomMax        = 30f;
+	[Export] public float sensitivityX   = 1f;
+	[Export] public float sensitivityY   = 1f;
+
+	private float targetZoom = 10f;
 
 	private Camera3D Camera;
 	private RayCast3D CollisionChecker;
+
+
+	private void UpdateCameraZoom()
+	{
+		float zoomLevel;
+		CollisionChecker.ForceRaycastUpdate();
+		if (CollisionChecker.IsColliding())
+		{
+			zoomLevel = (CollisionChecker.GetCollisionPoint() - Position).Length() - 2*Camera.Near;
+		}
+		else
+		{
+			zoomLevel = targetZoom;
+		}
+		Camera.Position = new Vector3(0, 0, zoomLevel);
+	}
+
 
 	public override void _Ready()
 	{
@@ -33,6 +53,7 @@ public partial class CameraPivot : Node3D
 				rotation += new Vector3(mouseMovement.Y * -0.005f * sensitivityY, mouseMovement.X * -0.005f * sensitivityX, 0);
 				rotation.X = Mathf.Clamp(rotation.X, -Mathf.Pi, Mathf.Pi);
 				Rotation = rotation;
+				UpdateCameraZoom();
 			}
 			else
 			{
@@ -44,24 +65,27 @@ public partial class CameraPivot : Node3D
 
     public override void _Process(double delta)
     {
-		float zoom_direction = 0f; // workaround because GetAxis didn't work for some reason
+		float zoomDirection = 0f; // workaround because GetAxis didn't work for some reason
 		if (Input.IsActionJustPressed("zoom_in"))
 		{
-			zoom_direction = -1f;
+			zoomDirection = -1f;
 		}
 		if (Input.IsActionJustPressed("zoom_out"))
 		{
-			zoom_direction += 1f;
+			zoomDirection += 1f;
 		}
-		if (zoom_direction != 0f)
+		if (zoomDirection != 0f)
 		{
-			float new_zoom = Camera.Position.Z + zoomSpeed * zoom_direction;
-			new_zoom = Mathf.Clamp(new_zoom, zoomMin, zoomMax);
-			Camera.Position = new Vector3(0, 0, new_zoom);
+			targetZoom += zoomSpeed * zoomDirection;
+			targetZoom = Mathf.Clamp(targetZoom, zoomMin, zoomMax);
+			GD.Print(targetZoom);
 		}
+		CollisionChecker.TargetPosition = new Vector3(0, 0, targetZoom);
+		UpdateCameraZoom();
+		
 
 		Vector3 position = Position;
-		Vector3 toTarget = target.Position - position;
+		Vector3 toTarget = target.Position + new Vector3(0, verticalOffset, 0) - position;
 		position += 0.5f * toTarget;
 		Position = position;
     }
